88d53b69b33bf44ad56717efc4ed7689977cb2d7
Adrien Jarthon <jobs@adrienjarthon.com>
2012-05-01 16:54:20 +0200

    Sinatra app skeleton + envy14 artwork

---
+++ app.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby-local-exec
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'stylesheets'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
+get '/' do
+  haml :home
+end
+
+get '/screen.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :screen
+end
