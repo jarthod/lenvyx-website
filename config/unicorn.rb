8f9735824ea2c9b6df59bbbc3808b95389e9f55e
Adrien Jarthon <jobs@adrienjarthon.com>
2012-11-22 21:56:14 +0100

    remove unicorn config

--- config/unicorn.rb
+++
@@ -1,7 +0,0 @@
-# unicorn -c config/unicorn.rb
-
-rails_env = ENV['RACK_ENV'] || 'production'
-worker_processes 1
-preload_app true
-timeout 30
-listen '/home/deploy/lenvyx/shared/unicorn.sock', :backlog => 2048
