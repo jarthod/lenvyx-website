bcda02187030a7146cdf3c0f49ce05359b072bf4
Adrien Jarthon <jobs@adrienjarthon.com>
2013-01-20 17:22:39 +0100

    clean and update website

--- Gemfile
+++ Gemfile
@@ -3,13 +3,8 @@ gem 'sinatra', '1.3.2'
 gem 'haml', '~> 3.1'
 gem 'sass', '~> 3.1'
 gem 'compass', '~> 0.12'
-gem 'mongoid', '~> 3.0.0.rc'
-gem 'bson_ext'
-gem 'sinatra-xsendfile', '0.4.0'
 
 group :test, :development do
-  gem 'capistrano', '2.12.0'
-  gem 'capistrano_colors'
   gem 'rake', '~> 0.9.2'
   gem 'rspec', '2.10.0'
   gem 'capybara', '1.1.2'
--- app.rb
+++ app.rb
@@ -3,16 +3,6 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 require 'compass'
-require 'mongoid'
-require 'sinatra/xsendfile'
-
-# MongoDB connection
-Mongoid.load!('config/mongoid.yml')
-
-# lenvyx releases
-RELEASES = [
-  {:name => '1.0-alpha', :ubuntu => '11.04', :date => Date.civil(2012, 04, 25)}
-]
 
 # Haml & Sass setup
 configure do
@@ -25,39 +15,11 @@ configure do
   set :sass, Compass.sass_engine_options
 end
 
-# nginx X-Accel-Redirect setup
-configure :production do
-  Sinatra::Xsendfile.replace_send_file!
-  set :xsf_header, 'X-Accel-Redirect'
-end
-
-# downloads log
-class Download
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  field :release, :type => String
-end
-
 get '/' do
-  if request.host.split('.').size > 2 and not development?
-    redirect 'http://lenvyx.com'
-  else
-    haml :home
-  end
+  haml :home
 end
 
 get '/screen.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :screen
-end
-
-get '/download/?:version?' do |version|
-  if release = (version ? RELEASES.select {|r| r[:name] == version} : RELEASES).last
-    # log download time for statistics
-    Download.create :release => release[:name]
-    file = File.join(settings.public_folder, 'releases', "lenvyx-#{release[:name]}.iso")
-    send_file file, :disposition => 'attachment'
-  else
-    404
-  end
 end
