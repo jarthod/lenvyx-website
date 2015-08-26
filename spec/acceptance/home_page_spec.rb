61267cd1f12d0949577a0cadd27069b4e6bb159c
Adrien Jarthon <jobs@adrienjarthon.com>
2015-08-26 21:26:18 +0200

    gem update and heroku upgrade to cedar-14

--- spec/acceptance/home_page_spec.rb
+++ spec/acceptance/home_page_spec.rb
@@ -3,14 +3,14 @@ require File.dirname(__FILE__) + '/../acceptance_helper'
 describe 'home page' do
   it "shows download links" do
     visit '/'
-    page.should have_selector('#iso a.button', :text => 'Download ISO')
-    page.should have_selector('#patch a.button', :text => 'Instructions')
+    expect(page).to have_selector('#iso a.button', :text => 'Download ISO')
+    expect(page).to have_selector('#patch a.button', :text => 'Instructions')
   end
 end
 
 describe 'stylesheet' do
   it "is returned" do
     visit '/screen.css'
-    page.status_code.should == 200
+    expect(page.status_code).to eq(200)
   end
 end
